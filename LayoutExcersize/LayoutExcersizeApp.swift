6784989eb6125d848634c3df1439952a6664ac70
Luke Newbigging <lnewbigging@lcs.on.ca>
2021-10-26 14:16:34 -0400

    Initial Commit

---
+++ LayoutExcersize/LayoutExcersizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutExcersizeApp.swift
+//  LayoutExcersize
+//
+//  Created by Luke Newbigging on 2021-10-26.
+//
+
+import SwiftUI
+
+@main
+struct LayoutExcersizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
